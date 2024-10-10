369a88cac042eae33b9281b2bc00a74c609f21ac
Abhishek7632 <abhishek180702@gmail.com>
2024-10-10 16:08:52 +0530

    Project is completed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,11 +2,11 @@ import "./Card.css";
 import { FaCircle } from "react-icons/fa";
 import UserIcon from "../UserIcon/UserIcon";
 
-const Card = ({ ticket, user, icon, statusIcon, statusColor, bgColor }) => {
+const Card = ({ ticket, user, icon, statusIcon, statusColor, bgColor, featureRequest }) => {
   const initials = user?.name
     .split(" ")
     .map((word) => word[0])
-    .join(""); // Renamed from 'userIntials'
+    .join("");
 
   const displayStatusIcon = statusIcon ? (
     <span style={{ color: statusColor }}>{statusIcon}</span>
@@ -16,35 +16,38 @@ const Card = ({ ticket, user, icon, statusIcon, statusColor, bgColor }) => {
     <div className="card">
       <div className="card_header">
         <p className="card_id" style={{color:"grey"}}>{ticket?.id}</p>
-        {/* Display UserIcon only if user exists */}
         {user && (
           <UserIcon
-            intials={initials}
+            initials={initials}  // Fixed prop name
             available={user?.available}
             bgColor={bgColor}
           />
         )}
       </div>
       <div className="card_info">
-        {/* Display the status icon if available */}
         {displayStatusIcon}
         <p>{ticket?.title}</p>
       </div>
       <div className="card_footer">
         {icon && (
           <div>
-            {/* Display the icon */}
             {icon}
           </div>
         )}
         <div className="card_tag">
           <FaCircle />
-          {/* Mapping through tags with subtle variable name changes */}
-          {ticket?.tag.map((tag, index) => (
+          {ticket?.tag?.map((tag, index) => (
             <p key={index}>{tag}</p>
           ))}
         </div>
       </div>
+
+      {/* Adding the Feature Request here */}
+      {featureRequest && (
+        <div className="card_feature_request">
+          <p>{featureRequest}</p>
+        </div>
+      )}
     </div>
   );
 };
